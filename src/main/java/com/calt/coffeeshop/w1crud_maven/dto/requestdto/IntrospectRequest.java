a646581557dabb6bc0af9a1a0dbf208dcbbde840
Khanh Nguyen Trung <khanhnt@imt-soft.com>
2026-03-12 01:22:05 +0700

    add introspect

---
+++ src/main/java/com/calt/coffeeshop/w1crud_maven/dto/requestdto/IntrospectRequest.java
@@ -0,0 +1,17 @@
+package com.calt.coffeeshop.w1crud_maven.dto.requestdto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class IntrospectRequest {
+    private String token;
+    private boolean valid;
+
+}
---
+++ src/main/java/com/calt/coffeeshop/w1crud_maven/dto/responsedto/IntrospectResponse.java
@@ -0,0 +1,13 @@
+package com.calt.coffeeshop.w1crud_maven.dto.responsedto;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class IntrospectResponse {
+    boolean valid;
+}
